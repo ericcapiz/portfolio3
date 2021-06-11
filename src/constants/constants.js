00ef8871a5344e1912c73b64158ee11f57390548
ericcapiz <ericcapiz@gmail.com>
2021-06-11 16:00:33 -0500

    update

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,7 +3,7 @@ export const projects = [
   {
     title: 'Sigma Snap',
     description: "This app will allow users to register/log-in and upload a post that will contain an image, title, message and tags. Each post will render the users name and display how long ago the post was created.",
-      image: '../images/snaps.png',
+      image: '../images/snaps.PNG',
       tags: ['Mongo', 'Express', 'React', 'Node'],
     source: 'https://github.com/ericcapiz/Sigma-Snaps',
     visit: 'https://sigma-snaps.netlify.app/',
@@ -12,7 +12,7 @@ export const projects = [
   {
     title: 'Psi-Shop',
     description:"This app will allow users to add items to their cart. Once in the cart users can increase/decrease quantity of items in the cart. Users can also remove the item or even empty the cart altogether.",
-    image: '../images/psi.png',
+    image: '../images/psi.PNG',
     tags: ['React', 'Commerce.js', 'Stripe'],
     source: 'https://github.com/ericcapiz/psi-shop',
     visit: 'https://psi-shop.netlify.app/',
@@ -21,7 +21,7 @@ export const projects = [
   {
     title: 'Express Diary',
     description: "This app is primarily backend related. Users can Add new entries to the diary, update and delete as well. Future features will allow users to log in and view their own entries.",
-      image: '../images/express.png',
+      image: '../images/express.PNG',
       tags: ['EJS', 'Express', 'Node', 'Mongo'],
     source: 'https://github.com/ericcapiz/express-demo',
     visit: 'https://expressjs-diary.herokuapp.com/',
@@ -30,7 +30,7 @@ export const projects = [
   {
     title: 'Zeta Films',
     description: "This app will allow users to search for any movie and save the movie or add it to the thier watched list. Users can move movies back and forth from saved to watched and vice versa as well as delete the movie from either list.",
-    image: '../images/films.png',
+    image: '../images/films.PNG',
     tags: ['React', 'Context', 'JWT'],
     source: 'https://github.com/ericcapiz/zeta-films',
     visit: 'https://zeta-films.vercel.app/',
@@ -40,7 +40,7 @@ export const projects = [
   {
     title: 'Covid Tracker',
     description: "This app uses the disease.sh api. This gives data that shows users the different numbers worldwide and by country. The numbers update as the api updates.",
-    image: '../images/covid.png',
+    image: '../images/covid.PNG',
     tags: ['React', 'Chart.js', 'Leaflet'],
     source: 'https://github.com/ericcapiz/covid',
     visit: 'https://covid-vert-zeta.vercel.app/',
@@ -49,7 +49,7 @@ export const projects = [
   {
     title: 'Nandos Cakes',
     description: "I developed this site for a chicago start-up bakery business. They have a fb channel with over 350 followers and I offered my services to help market the business by creating their site.",
-    image: '../images/cake.png',
+    image: '../images/cake.PNG',
     tags: ['React', 'Email.js', 'Framer-Motion'],
     source: 'https://github.com/ericcapiz/nandos-cakes2',
     visit: 'https://nandos-cakes2.vercel.app/',
